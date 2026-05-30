eec6c1e050e9fb8b6fd9a87bd3719d8b3be3d4e9
hrxstik <133148472+hrxstik@users.noreply.github.com>
2026-05-31 00:50:11 +0500

    Update logo.tsx

--- frontend/components/shared/logo.tsx
+++ frontend/components/shared/logo.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import React from 'react';
-import { cn } from '@/lib/utils';
+import Link from "next/link";
+import React from "react";
+import { cn } from "@/lib/utils";
 
 interface Props {
   className?: string;
@@ -8,11 +8,15 @@ interface Props {
 
 export const Logo: React.FC<Props> = ({ className }) => {
   return (
-    <Link href="/" className={cn('group flex items-center space-x-2', className)}>
-      <img src="/logo.png" alt="Логотип платформы" className="h-8 w-8 rounded-full" />
-      <span className="dark:text-primary text-primary hover:text-accent-foreground dark:hover:text-accent-foreground duration-100 font-bold text-xl">
-        Шахматная платформа
-      </span>
+    <Link
+      href="/"
+      className={cn("group flex items-center space-x-2", className)}
+    >
+      <img
+        src="/logo.png"
+        alt="Логотип платформы"
+        className="h-8 w-8 rounded-full"
+      />
     </Link>
   );
 };
